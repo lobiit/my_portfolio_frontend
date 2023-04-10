1897e0b46f0b724fdf52dbbd431b360667f3a0cc
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-10 18:15:45 +0300

    logo removal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import { images } from "../../constants";
 import './Navbar.scss';
 import {HiMenuAlt4, HiX} from "react-icons/hi";
 import { motion } from "framer-motion";
