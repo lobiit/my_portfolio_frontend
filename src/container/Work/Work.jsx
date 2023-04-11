93c9c1c3bbbbfa6813c45c6b72753275a0d31f18
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-11 11:48:04 +0300

    created the work component and added some styling, also added a high order component i.e motion wrap

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import './Work.scss';
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-import {client, urlFor} from "../../client";
+import { client, urlFor } from "../../client";
+import { AppWrap, MotionWrap } from "../../Wrapper";
+import './Work.scss';
+
 
 const Work = (props) => {
     const [activeFilter, setActiveFilter]=useState('All');
@@ -9,14 +12,14 @@ const Work = (props) => {
     const [works, setWorks] = useState([]);
     const [filterWork, setFilterWork] = useState([]);
     useEffect(() => {
-        const query = `[_type == 'works']`;
+        const query = `*[_type == 'works']`;
 
         client.fetch(query)
             .then((data) => {
                 setWorks(data);
                 setFilterWork(data);
-            })
-    }, [])
+            });
+    }, []);
     const handleWorkFilter = (item) => {
         setActiveFilter(item);
         setAnimateCard([{ y: 100, opacity: 0 }]);
@@ -41,7 +44,8 @@ const Work = (props) => {
           <div className="app__work-filter">
               {['Web App', 'React Js', 'Django', 'All'].map((item, index) =>(
                   <div
-                    key={() => handleWorkFilter(item)}
+                    key={index}
+                    onClick={() => handleWorkFilter(item)}
                     className={`app__work-filter-item app__flex p-text ${activeFilter === item? 'item-active': ''}`}
                   >
                       {item}
@@ -57,7 +61,43 @@ const Work = (props) => {
                   <div className="app__work-item app__flex" key={index}>
                     <div className="app__work-img app__flex">
                         <img src={urlFor(work.imgUrl)} alt={work.name}/>
+                        <motion.div
+                            whileHover={{ opacity: [0, 1] }}
+                            transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                            className="app__work-hover app__flex"
+                        >
+                            <a href={work.projectLink} target="_blank" rel="noreferrer">
+
+                                <motion.div
+                                    whileInView={{ scale: [0, 1] }}
+                                    whileHover={{ scale: [1, 0.90] }}
+                                    transition={{ duration: 0.25 }}
+                                    className="app__flex"
+                                >
+                                    <AiFillEye />
+                                </motion.div>
+                            </a>
+                            <a href={work.codeLink} target="_blank" rel="noreferrer">
+                                <motion.div
+                                    whileInView={{ scale: [0, 1] }}
+                                    whileHover={{ scale: [1, 0.90] }}
+                                    transition={{ duration: 0.25 }}
+                                    className="app__flex"
+                                >
+                                    <AiFillGithub />
+                                </motion.div>
+                            </a>
+                        </motion.div>
                     </div>
+
+                      <div className="app__work-content app__flex">
+                          <h4 className="bold-text">{work.title}</h4>
+                          <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+                          <div className="app__work-tag app__flex">
+                              <p className="p-text">{work.tags[0]}</p>
+                          </div>
+                      </div>
                   </div>
               ))}
           </motion.div>
@@ -65,4 +105,8 @@ const Work = (props) => {
     );
 };
 
-export default Work;
+export default AppWrap(
+    MotionWrap(Work, 'app__works'),
+    'work',
+    'app__primarybg',
+);
